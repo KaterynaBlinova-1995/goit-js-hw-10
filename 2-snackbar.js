e0f0b64bd7ca9a27d940976bf9d24ad9f6a714ce
KaterynaBlinova-1995 <catalina.ova2709@gmail.com>
2025-08-12 19:03:47 +0000

    Deploying to gh-pages from @ KaterynaBlinova-1995/goit-js-hw-10@70699beef6995d28bf8b4945b2a9ade372d23c4c 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const o=document.querySelector(".js-form");console.log(o);o.addEventListener("submit",l);function l(t){t.preventDefault();const e=new FormData(t.target),n=e.get("delay"),a=e.get("state");console.log(n),console.log(a)}
+//# sourceMappingURL=2-snackbar.js.map

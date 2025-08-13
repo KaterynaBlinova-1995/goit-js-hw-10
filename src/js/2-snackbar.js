8a3ed75940511b3613e0fccaebd03d2ba230d250
KaterynaBlinova-1995 <catalina.ova2709@gmail.com>
2025-08-13 15:22:48 +0300

    all files

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,5 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 const formElem = document.querySelector('.js-form');
 console.log(formElem);
 formElem.addEventListener('submit', handleFormSubmit);
@@ -8,4 +10,37 @@ function handleFormSubmit(e) {
   const state = formData.get('state');
   console.log(delay);
   console.log(state);
+
+  const isActive = state === 'fulfilled';
+  createPromise(delay, isActive)
+    .then(result => {
+      iziToast.show({
+        message: result,
+        position: 'topRight',
+        backgroundColor: 'teal',
+        messageColor: 'white',
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        message: error,
+        position: 'topRight',
+        backgroundColor: 'tomato',
+        messageColor: 'white',
+      });
+    });
+}
+
+function createPromise(delay, isActive) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isActive) {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  return promise;
 }

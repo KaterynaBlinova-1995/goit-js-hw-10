a7296acbfcca5ad256c44739704b6c5e9ca7980c
KaterynaBlinova-1995 <catalina.ova2709@gmail.com>
2025-08-14 01:03:05 +0300

    all files

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,17 +2,22 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 let userSelectedDate;
+let intervalId = null;
+
 const buttonElem = document.querySelector('[data-start]');
 const spanElems = document.querySelectorAll('.timer span.value');
+const dateTimePicker = document.querySelector('#datetime-picker');
 
-flatpickr('#datetime-picker', {
+buttonElem.disabled = true;
+
+const fp = flatpickr(dateTimePicker, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
     const selectedDate = selectedDates[0];
 
     if (selectedDate < Date.now()) {
@@ -23,45 +28,56 @@ flatpickr('#datetime-picker', {
       buttonElem.disabled = true;
     } else {
       buttonElem.disabled = false;
+      userSelectedDate = selectedDate;
     }
-
-    userSelectedDate = selectedDate;
   },
 });
+
 buttonElem.addEventListener('click', handleButtonClick);
-function handleButtonClick(e) {
-  const intervalIld = setInterval(() => {
+
+function handleButtonClick() {
+  if (!userSelectedDate) return;
+
+  buttonElem.disabled = true;
+  dateTimePicker.disabled = true;
+
+  intervalId = setInterval(() => {
     const diff = userSelectedDate - Date.now();
+
+    if (diff < 0) {
+      clearInterval(intervalId);
+
+      dateTimePicker.disabled = false;
+      return;
+    }
+
     const timeObject = convertMs(diff);
-    console.log(timeObject);
     spanElems[0].textContent = addLeadingZero(timeObject.days);
     spanElems[1].textContent = addLeadingZero(timeObject.hours);
     spanElems[2].textContent = addLeadingZero(timeObject.minutes);
     spanElems[3].textContent = addLeadingZero(timeObject.seconds);
 
     if (diff < 1000) {
-      clearInterval(intervalIld);
+      clearInterval(intervalId);
+      dateTimePicker.disabled = false;
     }
   }, 1000);
 }
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
+
 function addLeadingZero(value) {
-  return value.toString().padStart(2, 0);
+  return value.toString().padStart(2, '0');
 }
